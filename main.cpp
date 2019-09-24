0076f005a9cec7b0a04ec61d8fd290126a1b95d6
Bernardo Cuteri <bernardo.cuteri@gmail.com>
2019-09-24 12:33:24 +0200

    Multiplication function implemented

--- main.cpp
+++ main.cpp
@@ -4,6 +4,10 @@ int add(int a, int b) {
     return a+b;
 }
 
+int multiply(int a, int b) {
+    return a*b;
+}
+
 int main() {
     std::cout<<"Welcome to the course"<<std::endl;
     std::cout<<"Hello world"<<std::endl;
